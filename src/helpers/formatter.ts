930aaf0f22ec3bd7a92674df5b20d22ce339f056
jonada182 <jonada182@hotmail.com>
2023-04-10 01:54:21 -0400

    Add comments to formatter

--- src/helpers/formatter.ts
+++ src/helpers/formatter.ts
@@ -9,6 +9,7 @@ export const formatHTML = (text: string, formatURLS = false): string => {
   for (let line of lines) {
     line = line.trim();
     if (line.startsWith("-")) {
+      // Format unordered lists as <ul>
       if (!isList) {
         isList = true;
         listTag = "ul";
@@ -16,6 +17,7 @@ export const formatHTML = (text: string, formatURLS = false): string => {
       }
       formattedText += `<li>${ line.slice(1).trim() }</li>`;
     } else if (line.match(/^\d+[\.\)]/)) {
+      // Format ordered lists as <ol> Eg. 1. or 1)
       if (!isList) {
         isList = true;
         listTag = "ol";
@@ -23,23 +25,29 @@ export const formatHTML = (text: string, formatURLS = false): string => {
       }
       formattedText += `<li>${ line.replace(/^\d+[\.\)]/, "").trim() }</li>`;
     } else {
+      // Close <ul> and <ol> tags
       if (isList) {
         isList = false;
         formattedText += `</${ listTag }>`;
       }
+      // Format any other lines
       if (line !== "") {
         if (line.match(/(http:\/\/[^\s]+)/g) || line.match(/(https:\/\/[^\s]+)/g)) {
+          // Format links as <a>
           if (formatURLS) {
             line = line.replace(/(http:\/\/[^\s]+)/g, `<a href="$1" target="_blank">$1</a>`);
             line = line.replace(/(https:\/\/[^\s]+)/g, `<a href="$1" target="_blank">$1</a>`);
           }
         } else if (line.match(/^[A-Z][\w\s]*:/)) {
+          // Format titles as bold Eg. "This is a title:"
           line = `<strong class="font-bold">${ line }</strong>`;
         }
         formattedText += `<p class="mb-4">${ line }</p>`;
       }
     }
   }
+
+  // Sanitize the formatted text by allowed tags/attributes
 
   const sanitizedHTML = DOMPurify.sanitize(formattedText, {
     ALLOWED_TAGS: ["br", "ul", "ol", "li", "p", "strong", "a"],
