901c00cb97c21bd15eeffe5408ee49d8a9ae2d9c
jonada182 <jonada182@hotmail.com>
2023-05-22 19:44:44 -0400

    Fix circleci config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const definedVariables = {};
-  definedVariables["process.env.FIREBASE_API_KEY"] = `"${env.FIREBASE_API_KEY}"`;
+  if (mode === "development") {
+    definedVariables["process.env.FIREBASE_API_KEY"] = `"${env.FIREBASE_API_KEY}"`;
+  }
 
   return {
     plugins: [react()],
